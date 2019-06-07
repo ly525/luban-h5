02d3ab2f872d22cdd9954c87e5100a1a5402fbb1
ly525 <lantern.done@gmail.com>
2019-06-07 17:32:30 +0800

    chore: add package.json

--- front-end/h5/src/main.js
+++ front-end/h5/src/main.js
@@ -5,6 +5,8 @@ import store from './store/'
 import './registerServiceWorker'
 import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
+// !#zh 请注意，务必使用 font-awesome@4.7.0 版本
+import 'font-awesome/css/font-awesome.min.css'
 
 Vue.config.productionTip = false
 Vue.use(Antd)
