ba00ddd01699db6936d087d8f0c85135d07f0bf4
ly525 <lantern.done@gmail.com>
2019-06-07 17:48:49 +0800

    chore: replace ant-design-vue with element-ui

--- front-end/h5/src/main.js
+++ front-end/h5/src/main.js
@@ -3,13 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store/'
 import './registerServiceWorker'
-import Antd from 'ant-design-vue'
-import 'ant-design-vue/dist/antd.css'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 // !#zh 请注意，务必使用 font-awesome@4.7.0 版本
 import 'font-awesome/css/font-awesome.min.css'
 
 Vue.config.productionTip = false
-Vue.use(Antd)
+Vue.use(ElementUI)
 
 new Vue({
   router,
