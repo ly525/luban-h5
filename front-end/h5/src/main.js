ffebb06bce648629db303141610cbf70120ee2ba
ly525 <lantern.done@gmail.com>
2019-07-14 18:03:54 +0800

    build: disable service worker

--- front-end/h5/src/main.js
+++ front-end/h5/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store/'
-import './registerServiceWorker'
+// import './registerServiceWorker'
 // import ElementUI from 'element-ui'
 import Antd from 'ant-design-vue'
 
