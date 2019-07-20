d68a4bfe47df62f1cddc1bcacbaa780929b8e591
ly525 <lantern.done@gmail.com>
2019-07-20 19:20:33 +0800

    update work model for saving to db

--- front-end/h5/src/components/core/models/work.js
+++ front-end/h5/src/components/core/models/work.js
@@ -3,12 +3,13 @@ class Work {
     this.title = work.title || '标题'
     this.description = work.description || '描述'
     this.pages = work.pages || []
-    this.type = work.type || 'h5'
 
-    this.id = this.id
-    this.key = this.key
+    // this.id = this.id
+    // TODO 用id 并不是一个好办法，有心人会得知整个系统中共有多少作品等额外信息，尽量防止信息泄漏
+    // this.key = this.key
     this.cover_image_url = ''
-    this.project_id = 1
+    // TODO 后期可以添加一个类似项目组的概念，每个项目组下可以有多个作品
+    // this.project_id = 1
     this.is_publish = false
     this.create_time = new Date()
     this.update_time = new Date()
