5db8d3522413bd4f437d27d6ab48d03e4ff2a74e
ly525 <lantern.done@gmail.com>
2019-10-02 09:32:34 +0800

    chore(engine) build engine with vue.config

--- front-end/h5/vue.config.js
+++ front-end/h5/vue.config.js
@@ -1,30 +1,64 @@
+const path = require('path')
 const target = 'http://localhost:1337'
+const engineOutputDir = path.join(__dirname, '../../back-end/h5-api/public/engine-assets')
+
+let page
+switch (process.env.PAGE) {
+  case 'ENGINE':
+    page = {
+      entry: 'src/engine-entry.js',
+      outputDir: engineOutputDir
+    }
+    break
+  case 'EDITOR':
+  default:
+    page = {
+      entry: 'src/main.js',
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: 'Index Page',
+      outputDir: 'dist'
+    }
+}
+
+const configureWebpack = {
+  resolve: {
+    alias: {
+      '@': path.join(__dirname, 'src')
+    }
+  }
+}
+
 module.exports = {
-  runtimeCompiler: true,
-  productionSourceMap: false,
+  outputDir: page.outputDir,
+  lintOnSave: process.env.NODE_ENV !== 'production',
+  productionSourceMap: process.env.NODE_ENV !== 'production',
+  pages: { index: page },
   devServer: {
     proxy: {
-      '/works': {
-        target,
-        changeOrigin: true,
-        ws: false
-      },
-      '/upload': {
+      '^/upload|content-manager|users-permissions|works|admin|psd-files|workforms|third-libs|engine-assets/': {
         target,
         changeOrigin: true,
         ws: false
       }
     }
   },
-  pages: {
-    index: {
-      entry: 'src/main.js',
-      template: 'public/index.html',
-      filename: 'index.html',
-      title: 'Index Page'
+  configureWebpack,
+  css: {
+    loaderOptions: {
+      stylus: {
+        'resolve url': true,
+        'import': [
+          // './src/theme'
+        ]
+      }
+    },
+    extract: false
+  },
+  pluginOptions: {
+    'cube-ui': {
+      postCompile: true,
+      theme: true
     }
-    // engine: {
-    //   entry: 'src/engine-entry.js'
-    // }
   }
 }
