f9342b5e334d5b9608870851ed7e89738cf6bb71
ly525 <lantern.done@gmail.com>
2019-10-27 22:36:22 +0800

    chore: update editor deploy script

--- front-end/h5/deploy.sh
+++ front-end/h5/deploy.sh
@@ -1,26 +1,2 @@
 #!/usr/bin/env sh
-# deploy guide: https://cli.vuejs.org/guide/deployment.html#platform-guides
-
-# abort on errors
-set -e
-
-# build
-npm run build
-
-# navigate into the build output directory
-cd dist
-
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
-git init
-git add -A
-git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:ly525/luban-h5.git master:gh-pages
-
-cd -
+scp -r dist/* centos@aliyun:/home/centos/codebase/luban/luban-h5-dist/front-end
