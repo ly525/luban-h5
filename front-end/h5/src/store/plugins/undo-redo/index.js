0f4a2cbcd85e744539bcac6b3fc6a820a389e9a2
ly525 <lantern.done@gmail.com>
2019-12-01 00:07:07 +0800

    fix(editor): update  mutation type for undo-redo

--- front-end/h5/src/store/plugins/undo-redo/index.js
+++ front-end/h5/src/store/plugins/undo-redo/index.js
@@ -5,7 +5,11 @@ import undoRedoHistory from './History'
  * #!zh: setElementCommonStyle 因为是在 mousemove 时候触发的，执行过于频繁，没有必要计入history，因此需要过滤。
  * 主要记录：拖动完成时候(mouseup)时候的位置、删除元素之前的状态等
  */
-const unRecordHistoryMutationTypes = ['editor/setElementCommonStyle']
+// const unRecordHistoryMutationTypes = ['editor/setElementCommonStyle']
+const recordHistoryMutationTypes = [
+  'editor/recordRect',
+  'editor/elementManager'
+]
 
 const undoRedoPlugin = (store) => {
   // initialize and save the starting stage
@@ -15,7 +19,7 @@ const undoRedoPlugin = (store) => {
 
   store.subscribe((mutation, state) => {
     const { type } = mutation
-    if (unRecordHistoryMutationTypes.includes(type)) return
+    if (!recordHistoryMutationTypes.includes(type)) return
     // is called AFTER every mutation
     undoRedoHistory.addState(cloneDeep(state))
   })
