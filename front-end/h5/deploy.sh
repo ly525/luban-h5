a4e48879c4374a1137c84957a175dc71755ce6bd
ly525 <lantern.done@gmail.com>
2020-01-05 18:25:26 +0800

    chore: add deploy preview engine command

--- front-end/h5/deploy.sh
+++ front-end/h5/deploy.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env sh
 scp -r dist/* centos@aliyun:/home/centos/codebase/luban/luban-h5-dist/front-end
+scp -r ../../back-end/h5-api/public/engine-assets/* centos@aliyun:/home/centos/codebase/luban/luban-h5/back-end/h5-api/public/engine-assets/
