736b0e40603725dde4e1e1526649bb2f4d5b9ae5
hakubox <lidian_226@163.com>
2020-06-04 08:07:45 +0800

    图片组件新增填充方式属性

--- front-end/h5/src/components/plugins/lbp-picture.js
+++ front-end/h5/src/components/plugins/lbp-picture.js
@@ -4,10 +4,27 @@ import placeholderImg from './lbp-picture-placeholder.png' // issue #34
 export default {
   name: 'lbp-picture',
   render () {
-    return <img src={this.imgSrc || placeholderImg} alt="" srcset="" width="100%" />
+    return <img src={this.imgSrc || placeholderImg} style={{ objectFit: this.fillType }} alt="" srcset="" width="100%" />
   },
   props: {
-    imgSrc: PropTypes.image()
+    imgSrc: PropTypes.image(),
+    fillType: {
+      type: String,
+      default: 'contain',
+      editor: {
+        type: 'a-select',
+        label: '填充方式',
+        props: {
+          options: [
+            { label: 'contain 短边缩放', value: 'contain' },
+            { label: 'cover 长边缩放', value: 'cover' },
+            { label: 'fill 拉伸', value: 'fill' },
+            { label: 'none 原始', value: 'none' },
+            { label: 'scale-down 弹性缩放', value: 'scale-down' }
+          ]
+        }
+      }
+    }
   },
   data: () => ({
     placeholderImg
