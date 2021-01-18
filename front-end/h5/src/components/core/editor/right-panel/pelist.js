fca0467ae21b7dd55c615f015ffc047b1eb80b8d
zhwm12 <zhwm12@sina.com>
2021-01-18 22:08:51 +0800

    Update pelist.js

--- front-end/h5/src/components/core/editor/right-panel/pelist.js
+++ front-end/h5/src/components/core/editor/right-panel/pelist.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
-import { message } from 'ant-design-vue'
+import Antd from 'ant-design-vue';
+// import { message } from 'ant-design-vue'
 import { mapState, mapActions } from 'vuex'
 import Page from 'core/models/page'
 import 'ant-design-vue/dist/antd.css'
@@ -14,7 +15,8 @@ export default {
       defaultExpandAllRows: false,
       pagination: false,
       pageElements: '',
-      rows: this.$store.state.editor.editingPage.elements,
+      tableScroll: {x:400, y:scrollY},
+      pageShowEnable: true,
       columns: [
   {
     dataIndex: 'uuid',
@@ -23,47 +25,124 @@ export default {
     className: 'notshow'
   },
   {
-    dataIndex: 'name',
-    key: 'name',
-    title: '名称',
-    width: '60px'
+    title: '序号',
+    customRender: (text, record, index) => `${index+1}`,
+    // ellipsis: true,
+    width: 32
   },
   {
-    dataIndex: 'customname',
-    key: 'customname',
+    dataIndex: 'ptn',
+    key: 'ptn',
+    title: '类别',
+    customRender: (text, record, index) => `${text}`,
+    // ellipsis: true,
+    width: 50
+  },
+  {
+    dataIndex: 'ppti',
+    key: 'ppti',
     title: '说明',
-    width: '100px'
+    // ellipsis: true,
+    width: 136,
+    scopedSlots: { customRender: 'ppti' }
   },
   {
-    dataIndex: 'initShow',
-    key: 'initShow',
-    title: '初显',
-    width: '30px'
+    dataIndex: 'csosn',
+    key: 'csosn',
+    title: '顺序',
+    width: 32,
+    // ellipsis: true,
+    scopedSlots: { customRender: 'csosn' }
   },
   {
-    dataIndex: 'hidden',
-    key: 'hidden',
+    dataIndex: 'csosne',
+    key: 'csosne',
+    title: '有序',
+    width: 32,
+    scopedSlots: { customRender: 'csosne' }
+  },
+  {
+    dataIndex: 'pse',
+    key: 'pse',
     title: '隐藏',
-    width: '30px'
+    width: 32,
+    scopedSlots: { customRender: 'pse' }
+  },
+  {
+    dataIndex: 'ste',
+    key: 'ste',
+    title: '显隐',
+    width: 32,
+    scopedSlots: { customRender: 'ste' }
+  },
+  {
+    dataIndex: 'de',
+    key: 'de',
+    title: '拖动',
+    width: 32,
+    scopedSlots: { customRender: 'de' }
+  },
+  {
+    dataIndex: '',
+    key: '',
+    title: ''
   }
 ]
 }
 },
  computed: {
-    ...mapState('editor', [
-      'editingPage'
-    ])
+    ...mapState('editor', {
+      editingPage: state => state.editingPage,
+      editingElement: state => state.editingElement,
+      elements: state => state.editingPage.elements,
+    }),
+    pesaList: function() {
+      let elementList = new Array()
+      let elements = this.elements
+      let len = elements.length
+      for(let i = 0; i < len; i++){
+        let element = {}
+        let item = elements[i]
+        let elementName = item['name'].replace('lbp-','')
+            element.ptn = elementName
+            element.uuid = item['uuid']
+            let pluginPropsText = item['pluginProps']['text']
+            if(pluginPropsText){
+              pluginPropsText = pluginPropsText.replace(/<[^>]*>|/g,"")
+            }
+            else{
+              pluginPropsText = ''
+            }
+          element.ci = 0
+          element.ppti = pluginPropsText
+          element.csosn = i
+          element.csosne = 0
+          element.pse = 1
+          element.ste = 0
+          element.de = 0
+          elementList.push(element)
+      }
+      return elementList
+    }
   },
   methods: {
   },
-  created () {},
+  created () {
+  },
 render (h) {
+  console.log(this.$scopedSlots)
     return (
             <div style={{ height: '100%', position: 'relative' }}>
               <a-form layout="vertical">
                   <a-form-item label="元素列表">
-                      <a-table columns={this.columns} dataSource={this.rows} rowKey="uuid" tableLayout="fixed" pagination={false} defaultExpandAllRows={false} style={{tableLayout: 'fixed',wordBreak: 'break-all'}} scopedSlots={{}}>
-                      </a-table>
+                      <a-table columns={this.columns} dataSource={this.pesaList} rowKey="uuid" tableLayout="fixed" scroll={this.tableScroll} pagination={false} defaultExpandAllRows={false} style={{tableLayout: 'fixed',wordBreak: 'break-all'}}>
+                        <span slot="ppti" slot-scope="text, record, index"><a-input type="text" v-decorator="['ppti', {}]"  placeholder="元素名称" ></a-input></span>
+                        <span slot="csosn" slot-scope="text, record, index">&nbsp;</span>
+                        <span slot="csosne" slot-scope="text, record, index"><a-switch size="small" default-checked /></span>
+                        <span slot="pse" slot-scope="text, record, index"><a-switch size="small" default-checked /></span>
+                        <span slot="ste" slot-scope="text, record, index"><a-switch size="small" default-checked /></span>
+                        <span slot="de" slot-scope="text, record, index"><a-switch size="small" default-checked /></span>
+                        </a-table>
                   </a-form-item>
               </a-form>
             </div>
