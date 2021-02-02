42ff459502e83cec49b1289b9558076077146780
zhwm12 <zhwm12@sina.com>
2021-02-02 11:24:01 +0800

    Update element.js

--- front-end/h5/src/components/core/store/modules/element.js
+++ front-end/h5/src/components/core/store/modules/element.js
@@ -18,6 +18,15 @@ export const actions = {
   },
   elementManager ({ commit }, payload) {
     commit('elementManager', payload)
+  },
+  pesaListElementChangeAttr ({ commit }, payload) {
+    commit('pesaListElementChangeAttr', payload)
+  },
+  pesaListElementChangeAttrPpti ({ commit }, payload) {
+    commit('pesaListElementChangeAttrPpti', payload)
+  },
+  pesaListElementRowDragDrog ({ commit }, payload) {
+    commit('pesaListElementRowDragDrog', payload)
   }
 }
 
@@ -35,6 +44,9 @@ export const mutations = {
   elementManager (state, { type, value }) {
     const { editingPage, editingElement } = state
     const elements = editingPage.elements
+    // const slidePresentation = editingPage.slidePresentation // wmhz
+    const pesaList = editingPage.slidePresentation.pesaList // wmhz
+    const pesaListLength = pesaList.length
     const len = elements.length
 
     switch (type) {
@@ -45,18 +57,72 @@ export const mutations = {
         vm.$options.dragStyle = value.dragStyle // {left: Number, top: Number}
         const element = new Element(vm.$options)
         elements.push(element)
+ // wmhz
+        let pluginPropsText = element['pluginProps']['text']
+        if (pluginPropsText) {
+          pluginPropsText = pluginPropsText.replace(/<[^>]*>|/g, '')
+          if (pluginPropsText.length > 10) {
+            pluginPropsText = pluginPropsText.substring(0, 8) + '...'
+          }
+        } else {
+          pluginPropsText = ''
+        }
+        let pesaListOf1 = {}
+        pesaListOf1.ptn = element['name'].replace('lbp-', '')
+        pesaListOf1.uuid = element['pluginProps']['uuid']
+        pesaListOf1.ci = 0 // count integer (index)
+        pesaListOf1.ppti = pluginPropsText
+        pesaListOf1.csosn = 0
+        pesaListOf1.csosne = 0
+        pesaListOf1.pse = 1
+        pesaListOf1.ste = 0
+        pesaListOf1.de = 0
+        pesaList.push(pesaListOf1)
         break
       case 'copy':
-        elements.push(state.editingElement.clone({ zindex: len + 1 }))
+        // wmhz modify
+        let editingElementClone = state.editingElement.clone({ zindex: len + 1 })
+        let pluginPropsTextClone = editingElementClone['pluginProps']['text']
+        if (pluginPropsTextClone) {
+          pluginPropsTextClone = pluginPropsTextClone.replace(/<[^>]*>|/g, '')
+          if (pluginPropsTextClone.length > 10) {
+            pluginPropsTextClone = pluginPropsTextClone.substring(0, 8) + '...'
+          }
+        } else {
+          pluginPropsTextClone = ''
+        }
+        let pesaListOf2 = {}
+        pesaListOf2.ptn = editingElementClone['name'].replace('lbp-', '')
+        pesaListOf2.uuid = editingElementClone['pluginProps']['uuid']
+        pesaListOf2.ci = 0
+        pesaListOf2.ppti = pluginPropsTextClone
+        pesaListOf2.csosn = 0
+        pesaListOf2.csosne = 0
+        pesaListOf2.pse = 1
+        pesaListOf2.ste = 0
+        pesaListOf2.de = 0
+        pesaList.push(pesaListOf2)
+        elements.push(editingElementClone)
         break
       case 'delete':
         {
           const index = elements.findIndex(e => e.uuid === editingElement.uuid)
+          const indexPesaList = pesaList.findIndex(e => e.uuid === editingElement.uuid) // wmhz
           if (index !== -1) {
             // let newElements = elements.slice()
             // newElements.splice(index, 1)
             // state.editingPage.elements = newElements
             state.editingPage.elements.splice(index, 1)
+            let deleteCsosn = state.editingPage.slidePresentation.pesaList[indexPesaList]['csosn']
+            if (deleteCsosn > 0) {
+              for (let i = 0; i < pesaListLength; i++) {
+                let csosn = state.editingPage.slidePresentation.pesaList[i]['csosn']
+                if (csosn > deleteCsosn) {
+                  state.editingPage.slidePresentation.pesaList[i].csosn = csosn - 1
+                }
+              }
+            }
+            state.editingPage.slidePresentation.pesaList.splice(indexPesaList, 1) // wmhz
           }
           state.editingElement = null
         }
@@ -78,7 +144,6 @@ export const mutations = {
         const maxZindex = elements.length
         const eleZindex = editingElement.commonStyle.zindex
         if (eleZindex === maxZindex || eleZindex === 1) return
-
         const flag = type === 'addZindex' ? 1 : -1
         const swapElement = elements.find(ele => ele.commonStyle.zindex === eleZindex + flag * 1)
         swapZindex(editingElement, swapElement)
@@ -88,5 +153,82 @@ export const mutations = {
   },
   recordRect (state, { type, value }) {
 
+  },
+  pesaListElementChangeAttr (state, payload) { // wmhz add  pesa List Element Change attribute
+    const { editingPage } = state // editingElement
+    const pesaList = editingPage.slidePresentation.pesaList
+    const pesaListLength = pesaList.length
+    const indexPesaList = pesaList.findIndex(e => e.uuid === payload.elementUuid)
+    switch (payload.elementAttrName) {
+      case 'csosne':
+        let beforeCsosn = state.editingPage.slidePresentation.pesaList[indexPesaList].csosn
+        state.editingPage.slidePresentation.pesaList[indexPesaList].csosne = payload.elementAttrValue === 1 ? 0 : 1
+        if (payload.elementAttrValue === 1) {
+          state.editingPage.slidePresentation.pesaList[indexPesaList].csosn = 0
+          for (let i = 0; i < pesaListLength; i++) {
+            let csosn = pesaList[i]['csosn']
+            if (csosn > 0 && csosn > beforeCsosn) {
+              state.editingPage.slidePresentation.pesaList[i].csosn = csosn - 1
+            }
+          }
+        } else {
+          let csosn1Length = 0
+          for (let item in pesaList) {
+            if (pesaList[item].csosn > 0) {
+              csosn1Length++
+            }
+          }
+          state.editingPage.slidePresentation.pesaList[indexPesaList].csosn = csosn1Length + 1
+          state.editingPage.slidePresentation.pesaList[indexPesaList].pse = 1
+        }
+        break
+      case 'pse':
+        let beforeCsosne = state.editingPage.slidePresentation.pesaList[indexPesaList].csosne
+        if (beforeCsosne === 0) {
+          state.editingPage.slidePresentation.pesaList[indexPesaList].pse = payload.elementAttrValue === 1 ? 0 : 1
+        }
+        if (beforeCsosne === 1 && payload.elementAttrValue === 0) {
+          state.editingPage.slidePresentation.pesaList[indexPesaList].pse = 1
+        }
+        break
+      case 'ste':
+        state.editingPage.slidePresentation.pesaList[indexPesaList].ste = payload.elementAttrValue === 1 ? 0 : 1
+        break
+      case 'de':
+        state.editingPage.slidePresentation.pesaList[indexPesaList].de = payload.elementAttrValue === 1 ? 0 : 1
+        break
+    }
+  },
+  pesaListElementChangeAttrPpti (state, payload) {
+    const { editingPage } = state
+    const pesaList = editingPage.slidePresentation.pesaList
+    const indexPesaList = pesaList.findIndex(e => e.uuid === payload.recordValue.uuid)
+    let pptiValueNew = payload.pptiValue.length < 50 ? payload.pptiValue : payload.pptiValue.substring(0, 50) + '...'
+    state.editingPage.slidePresentation.pesaList[indexPesaList].ppti = pptiValueNew
+  },
+  pesaListElementRowDragDrog (state, rowDragDrogPayload) {
+    const { editingPage } = state
+    const pesaList = editingPage.slidePresentation.pesaList
+    const pesaListLength = pesaList.length
+    const indexSourceRow = pesaList.findIndex(e => e.uuid + '' === rowDragDrogPayload.sourceRowUuid + '')
+    const indexTargetRow = pesaList.findIndex(e => e.uuid + '' === rowDragDrogPayload.targetRowUuid + '')
+    let sourceRowCsosn = rowDragDrogPayload.sourceRowCsosn
+    let targetRowCsosn = rowDragDrogPayload.targetRowCsosn
+    for (let i = 0; i < pesaListLength; i++) {
+      let csosn = pesaList[i]['csosn']
+      if (csosn > 0) {
+        if (sourceRowCsosn > targetRowCsosn) {
+          if (csosn > (targetRowCsosn - 1) && csosn < sourceRowCsosn) {
+            state.editingPage.slidePresentation.pesaList[i].csosn = csosn + 1
+          }
+        }
+        if (sourceRowCsosn < targetRowCsosn) {
+          if (csosn > sourceRowCsosn && csosn < targetRowCsosn + 1) {
+            state.editingPage.slidePresentation.pesaList[i].csosn = csosn - 1
+          }
+        }
+     }
+    }
+    state.editingPage.slidePresentation.pesaList[indexSourceRow].csosn = targetRowCsosn
   }
 }
