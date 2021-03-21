40a077096fc83cb198eb30e99502ac7063ad8ed1
ly525 <lantern.done@gmail.com>
2021-03-21 21:24:19 +0800

    chore: add icons

--- front-end/h5/src/components/core/plugins/lbp-text-tinymce.js
+++ front-end/h5/src/components/core/plugins/lbp-text-tinymce.js
@@ -25,7 +25,20 @@ export default {
     borderWidth: PropTypes.number({ label: '边框宽度(px)', defaultValue: 0 }),
     borderRadius: PropTypes.number({ label: '圆角(px)' }),
     borderColor: PropTypes.color({ label: '边框颜色' }),
-    text: PropTypes.string({ label: '内容', defaultValue: '双击修改文字', visible: true, component: 'tinymce-editor' }),
+    text: PropTypes.string({ label: '内容',
+      defaultValue: '双击修改文字',
+      visible: true,
+      component: 'tinymce-editor',
+      props: {
+        init: {
+          menubar: false,
+          toolbar:
+            `undo redo | formatselect | bold italic backcolor | \
+            alignleft aligncenter alignright alignjustify | \
+            bullist numlist outdent indent | removeformat | help`
+        }
+      }
+    }),
     editorMode: PropTypes.string({
       defaultValue: 'preview', // 可选值: preview/edit
       label: '模式',
